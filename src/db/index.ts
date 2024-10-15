200d4825c303459a00908aa66aeecd8d6d1f0da1
Pekka Lammi <haloo@pekkalammi.com>
2024-10-15 14:05:35 +0300

    port

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,6 +2,7 @@ import { Pool } from 'pg';
 
 const pool = new Pool({
     host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DATABASE,
